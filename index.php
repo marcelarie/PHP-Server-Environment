81d73ee093a88bea5c4f5f471d6bffed89cc1d04
marcelarie <googlillo@gmail.com>
2021-02-01 16:00:33 +0100

    Print $_SESSION content + change php.ini

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
-include 'server_variables.php';
+print "<link rel='stylesheet' href='css/style.css'>";
+// include 'server_variables.php';
+// include 'session_variables.php';
+include 'main.php';
 ?>
 
